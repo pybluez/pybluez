fdd78328accfc115d7674bea6895143598e5a9a6
ashuang <ashuang@bcb67f6a-c13e-0410-b0c7-e53ae835acad>
2008-10-29 17:31:28 +0000

    fix variable naming in inquiry.py example
    git-svn-id: https://pybluez.googlecode.com/svn/trunk@14 bcb67f6a-c13e-0410-b0c7-e53ae835acad

--- examples/simple/inquiry.py
+++ examples/simple/inquiry.py
@@ -13,5 +13,5 @@ nearby_devices = bluetooth.discover_devices(lookup_names = True)
 
 print "found %d devices" % len(nearby_devices)
 
-for name, addr in nearby_devices:
+for addr, name in nearby_devices:
     print "  %s - %s" % (addr, name)
