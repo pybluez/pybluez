20ea88af7ac8b62acbf2605da3d48777040ae912
ashuang <ashuang@bcb67f6a-c13e-0410-b0c7-e53ae835acad>
2010-04-29 20:16:38 +0000

    add non-rssi inquiry results in example (issue #29)
    git-svn-id: https://pybluez.googlecode.com/svn/trunk@31 bcb67f6a-c13e-0410-b0c7-e53ae835acad

--- examples/advanced/inquiry-with-rssi.py
+++ examples/advanced/inquiry-with-rssi.py
@@ -108,8 +108,16 @@ def device_inquiry_with_with_rssi(sock):
                 print "uh oh..."
                 printpacket(pkt[3:7])
                 done = True
+        elif event == bluez.EVT_INQUIRY_RESULT:
+            pkt = pkt[3:]
+            nrsp = struct.unpack("B", pkt[0])[0]
+            for i in range(nrsp):
+                addr = bluez.ba2str( pkt[1+6*i:1+6*i+6] )
+                results.append( ( addr, -1 ) )
+                print "[%s] (no RRSI)" % addr
         else:
             print "unrecognized packet type 0x%02x" % ptype
+	    print "event ", event
 
 
     # restore old filter
