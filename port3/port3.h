a054fabdc5ee90e845f0d813a73fe50a12feeb01
karulis@wp.pl <karulis@wp.pl@bcb67f6a-c13e-0410-b0c7-e53ae835acad>
2014-01-13 11:08:28 +0000

    Added missing python3 port files
    git-svn-id: https://pybluez.googlecode.com/svn/trunk@57 bcb67f6a-c13e-0410-b0c7-e53ae835acad

---
+++ port3/port3.h
@@ -0,0 +1,17 @@
+
+#if PY_MAJOR_VERSION >= 3
+    #define PyInt_FromLong PyLong_FromLong
+	#define PyString_FromString PyUnicode_FromString
+	#define PyString_FromString PyUnicode_FromString
+	#define PyString_FromStringAndSize PyBytes_FromStringAndSize
+	#define _PyString_Resize _PyBytes_Resize
+	#define PyString_AS_STRING PyBytes_AS_STRING
+	#define PyInt_AsLong PyLong_AsLong
+	#define PyString_AsString PyBytes_AsString
+	#define PyInt_Check PyLong_Check
+	#define PyInt_AS_LONG PyLong_AS_LONG
+#else
+	#ifndef Py_TYPE
+		#define Py_TYPE(ob) (((PyObject*)(ob))->ob_type)
+	#endif
+#endif
