af09fe74977121e8203c0dce007b1eb733943421
karulis <karulis@>
2015-04-25 22:57:42 +0200

    Indentation fix

--- port3/port3.h
+++ port3/port3.h
@@ -1,19 +1,19 @@
 
 #if PY_MAJOR_VERSION >= 3
     #define PyInt_FromLong PyLong_FromLong
-	#define PyString_FromString PyUnicode_FromString
-	#define PyString_FromString PyUnicode_FromString
-	#define PyString_FromStringAndSize PyBytes_FromStringAndSize
-	#define _PyString_Resize _PyBytes_Resize
-	#define PyString_AS_STRING PyBytes_AS_STRING
-	#define PyInt_AsLong PyLong_AsLong
-	#define PyString_AsString PyBytes_AsString
-	#define PyInt_Check PyLong_Check
-	#define PyInt_AS_LONG PyLong_AS_LONG
+    #define PyString_FromString PyUnicode_FromString
+    #define PyString_FromString PyUnicode_FromString
+    #define PyString_FromStringAndSize PyBytes_FromStringAndSize
+    #define _PyString_Resize _PyBytes_Resize
+    #define PyString_AS_STRING PyBytes_AS_STRING
+    #define PyInt_AsLong PyLong_AsLong
+    #define PyString_AsString PyBytes_AsString
+    #define PyInt_Check PyLong_Check
+    #define PyInt_AS_LONG PyLong_AS_LONG
     #define BYTES_FORMAT_CHR "y#"
 #else
-	#ifndef Py_TYPE
-		#define Py_TYPE(ob) (((PyObject*)(ob))->ob_type)
-	#endif
+    #ifndef Py_TYPE
+        #define Py_TYPE(ob) (((PyObject*)(ob))->ob_type)
+    #endif
     #define BYTES_FORMAT_CHR "s#"
 #endif
