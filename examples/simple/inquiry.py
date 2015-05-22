fed2e755c83d2c38f19e226575f92e22a686ca0b
karulis <karulis>
2015-05-22 13:33:06 +0200

    Fix in inquiry.py for name encoding issues

--- examples/simple/inquiry.py
+++ examples/simple/inquiry.py
@@ -15,5 +15,8 @@ nearby_devices = bluetooth.discover_devices(duration=2, lookup_names=True,
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+    try:
+        print("  %s - %s" % (addr, name))
+    except UnicodeEncodeError:
+        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
 
