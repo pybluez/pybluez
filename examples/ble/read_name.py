34faf39c0dc6409dc8463d02e3eb5beb3c5d38ad
karulis <me@wp.pl>
2015-08-26 10:35:36 -0500

    Fix in ble read_name.py script

--- examples/ble/read_name.py
+++ examples/ble/read_name.py
@@ -26,7 +26,10 @@ class Reader(object):
     def request_data(self):
         data = self.requester.read_by_uuid(
                 "00002a00-0000-1000-8000-00805f9b34fb")[0]
-        print("Device name: " + data.decode("utf-8"))
+        try:
+            print("Device name: " + data.decode("utf-8"))
+        except AttributeError:
+            print("Device name: " + data)
 
 
 if __name__ == '__main__':
