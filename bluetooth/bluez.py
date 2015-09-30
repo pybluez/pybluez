38634a16f8ecb2dbcac3e6cc4a12ec713d5f7b8b
karulis <me@w.pl>
2015-09-30 16:25:24 +0200

    Read local bt addres refactored for linux

--- bluetooth/bluez.py
+++ bluetooth/bluez.py
@@ -54,6 +54,32 @@ def discover_devices (duration=8, flush_cache=True, lookup_names=False,
         sock.close ()
         return results
 
+def read_local_bdaddr():
+    hci_sock = _bt.hci_open_dev(0)
+    old_filter = hci_sock.getsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, 14)
+    flt = _bt.hci_filter_new()
+    opcode = _bt.cmd_opcode_pack(_bt.OGF_INFO_PARAM, 
+            _bt.OCF_READ_BD_ADDR)
+    _bt.hci_filter_set_ptype(flt, _bt.HCI_EVENT_PKT)
+    _bt.hci_filter_set_event(flt, _bt.EVT_CMD_COMPLETE);
+    _bt.hci_filter_set_opcode(flt, opcode)
+    hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, flt )
+
+    _bt.hci_send_cmd(hci_sock, _bt.OGF_INFO_PARAM, _bt.OCF_READ_BD_ADDR )
+
+    pkt = hci_sock.recv(255)
+
+    status,raw_bdaddr = struct.unpack("xxxxxxB6s", pkt)
+    assert status == 0
+
+    t = [ "%X" % ord(get_byte(b)) for b in raw_bdaddr ]
+    t.reverse()
+    bdaddr = ":".join(t)
+
+    # restore old filter
+    hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, old_filter )
+    return bdaddr
+
 def lookup_name (address, timeout=10):
     if not is_valid_address (address): 
         raise BluetoothError ("%s is not a valid Bluetooth address" % address)
--- examples/advanced/read-local-bdaddr.py
+++ examples/advanced/read-local-bdaddr.py
@@ -1,45 +1,9 @@
 import os
 import sys
 import struct
-
-if sys.version < '3':
-    get_byte = str
-else:
-    get_byte = chr
-
-
-def read_local_bdaddr():
-    import bluetooth._bluetooth as _bt
-    hci_sock = _bt.hci_open_dev(0)
-    old_filter = hci_sock.getsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, 14)
-    flt = _bt.hci_filter_new()
-    opcode = _bt.cmd_opcode_pack(_bt.OGF_INFO_PARAM, 
-            _bt.OCF_READ_BD_ADDR)
-    _bt.hci_filter_set_ptype(flt, _bt.HCI_EVENT_PKT)
-    _bt.hci_filter_set_event(flt, _bt.EVT_CMD_COMPLETE);
-    _bt.hci_filter_set_opcode(flt, opcode)
-    hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, flt )
-
-    _bt.hci_send_cmd(hci_sock, _bt.OGF_INFO_PARAM, _bt.OCF_READ_BD_ADDR )
-
-    pkt = hci_sock.recv(255)
-
-    status,raw_bdaddr = struct.unpack("xxxxxxB6s", pkt)
-    assert status == 0
-
-    t = [ "%X" % ord(get_byte(b)) for b in raw_bdaddr ]
-    t.reverse()
-    bdaddr = ":".join(t)
-
-    # restore old filter
-    hci_sock.setsockopt( _bt.SOL_HCI, _bt.HCI_FILTER, old_filter )
-    return bdaddr
+import bluetooth
 
 
 if __name__ == "__main__":
-    try:
-        import bluetooth
-        bdaddr = bluetooth.read_local_bdaddr()        
-    except:
-        bdaddr = read_local_bdaddr()
+    bdaddr = bluetooth.read_local_bdaddr()
     print(bdaddr)
