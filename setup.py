pybluez/pybluez

commit b02e3121015eba58d49e1c03802b087210b8029d

Support for python 3.5

setup.py
@@ -13,6 +13,8 @@ def find_MS_SDK():
 
     if sys.version < '3.3':
         MS_SDK = r'Microsoft SDKs\Windows\v6.0A'  # Visual Studio 9
+    if sys.version >= '3.5':
+        MS_SDK = r'Microsoft SDKs\Windows\v10.0A'  # Visual Studio 14
     else:
         MS_SDK = r'Microsoft SDKs\Windows\v7.0A'  # Visual Studio 10
 

commit 4f947dca75897c173d851e0bdf27ea0115e7a0b4

fix broken install on Windows / 2.7 / VS9
Install is broken if you use python <3.3 and Visual Studio 9 on Windows (regression introduced with b02e3121).

- install Visual Studio 9 (`C:\Program Files\Microsoft SDKs\Windows\v6.0A\`)
- install Python 2.7
- python setup.py install

> Could not find the Windows Platform SDK

    $  git clone git@github.com:karulis/pybluez.git
    Cloning into 'pybluez'...
    remote: Counting objects: 1029, done.
    emote: Total 1029 (delta 0), reused 0 (delta 0), pack-reused 1029R
    Receiving objects: 100% (1029/1029), 389.54 KiB | 0 bytes/s, done.
    Resolving deltas: 100% (632/632), done.
    Checking connectivity... done.

    Thomas@Thomas-PC MINGW64 /c/tmp
    $  cd pybluez/

    Thomas@Thomas-PC MINGW64 /c/tmp/pybluez (master)
    $  git:(master) python
    Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
    Type "help", "copyright", "credits" or "license" for more information.
    >>>

    Thomas@Thomas-PC MINGW64 /c/tmp/pybluez (master)
    $  git:(master) file "C:/Program Files/Microsoft SDKs/Windows/v6.0A/"
    C:/Program Files/Microsoft SDKs/Windows/v6.0A/: directory

    Thomas@Thomas-PC MINGW64 /c/tmp/pybluez (master)
    $  git:(master) python setup.py install
    Could not find the Windows Platform SDK

setup.py
@@ -15,7 +15,7 @@ def find_MS_SDK():
 
     if sys.version < '3.3':
         MS_SDK = r'Microsoft SDKs\Windows\v6.0A'  # Visual Studio 9
-    if sys.version >= '3.5':
+    elif sys.version >= '3.5':
         MS_SDK = r'Microsoft SDKs\Windows\v10.0A'  # Visual Studio 14
     else:
         MS_SDK = r'Microsoft SDKs\Windows\v7.0A'  # Visual Studio 10