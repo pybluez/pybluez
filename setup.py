2dd1cca2698ced37c6a33c41f84f73e68d1888aa
Thomas LEVEIL <thomasleveil@gmail.com>
2015-12-10 22:41:37 +0100

    setup: refactor sys.version tests

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ def find_MS_SDK():
 
     if sys.version < '3.3':
         MS_SDK = r'Microsoft SDKs\Windows\v6.0A'  # Visual Studio 9
+    elif '3.3' <= sys.version < '3.5':
+        MS_SDK = r'Microsoft SDKs\Windows\v7.0A'  # Visual Studio 10
     elif sys.version >= '3.5':
         MS_SDK = r'Microsoft SDKs\Windows\v10.0A'  # Visual Studio 14
-    else:
-        MS_SDK = r'Microsoft SDKs\Windows\v7.0A'  # Visual Studio 10
 
     candidate_paths = (MS_SDK,
                        'Microsoft Platform SDK for Windows XP',
