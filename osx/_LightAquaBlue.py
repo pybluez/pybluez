d5e09dc3f941e035c7e99c6106dd0d628ac26c77
Ryan Govostes <rgovostes+git@gmail.com>
2017-11-15 14:38:19 +0900

    Build LightAquaBlue.framework inside the lightblue package

--- osx/_LightAquaBlue.py
+++ osx/_LightAquaBlue.py
@@ -25,36 +25,16 @@ classes through PyObjC.
 
 import objc
 import os.path
+from pkg_resources import resource_filename
 
-_FRAMEWORK_PATH = '/Library/Frameworks/LightAquaBlue.framework'
+_FRAMEWORK_PATH = resource_filename('lightblue', 'LightAquaBlue.framework')
 if not os.path.isdir(_FRAMEWORK_PATH):
-    raise ImportError("Cannot load LightAquaBlue framework, not found at" + \
+    raise ImportError("Cannot load LightAquaBlue framework, not found at " + \
         _FRAMEWORK_PATH)
 
-try:
-    # mac os 10.5 loads frameworks using bridgesupport metadata
-    __bundle__ = objc.initFrameworkWrapper("LightAquaBlue",
-            frameworkIdentifier="com.blammit.LightAquaBlue",
-            frameworkPath=objc.pathForFramework(_FRAMEWORK_PATH),
-            globals=globals())
-
-except AttributeError:
-    # earlier versions use loadBundle() and setSignatureForSelector()
-
-    objc.loadBundle("LightAquaBlue", globals(),
-       bundle_path=objc.pathForFramework(_FRAMEWORK_PATH))
-
-    # return int, take (object, object, object, output unsigned char, output int)
-    # i.e. in python: return (int, char, int), take (object, object, object)
-    objc.setSignatureForSelector("BBServiceAdvertiser",
-        "addRFCOMMServiceDictionary:withName:UUID:channelID:serviceRecordHandle:",
-        "i@0:@@@o^Co^I")
-
-    # set to take (6-char array, unsigned char, object)
-    # this seems to work even though the selector doesn't take a char aray,
-    # it takes a struct 'BluetoothDeviceAddress' which contains a char array.
-    objc.setSignatureForSelector("BBBluetoothOBEXClient",
-            "initWithRemoteDeviceAddress:channelID:delegate:",
-            '@@:r^[6C]C@')
+__bundle__ = objc.initFrameworkWrapper("LightAquaBlue",
+        frameworkIdentifier="com.blammit.LightAquaBlue",
+        frameworkPath=objc.pathForFramework(_FRAMEWORK_PATH),
+        globals=globals())
 
 del objc
--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ import sys
 import platform
 import os
 
-packs = ['bluetooth']
-pack_dir = dict()
-mods = list()
-install_req = list()
+packages = ['bluetooth']
+package_dir = dict()
+ext_modules = list()
+install_requires = list()
+package_data = dict()
+eager_resources = list()
+zip_safe = True
 
 
 def find_MS_SDK():
@@ -49,7 +52,7 @@ if sys.platform == 'win32':
     lib_path = os.path.join(PSDK_PATH, 'Lib')
     if '64' in platform.architecture()[0]:
         lib_path = os.path.join(lib_path, 'x64')
-    mods.append(Extension('bluetooth._msbt',
+    ext_modules.append(Extension('bluetooth._msbt',
                           include_dirs=["%s\\Include" % PSDK_PATH, ".\\port3"],
                           library_dirs=[lib_path],
                           libraries=["WS2_32", "Irprops"],
@@ -58,7 +61,7 @@ if sys.platform == 'win32':
     # widcomm
     WC_BASE = os.path.join(os.getenv('ProgramFiles'), r"Widcomm\BTW DK\SDK")
     if os.path.exists(WC_BASE):
-        mods.append(Extension('bluetooth._widcomm',
+        ext_modules.append(Extension('bluetooth._widcomm',
                 include_dirs=["%s\\Inc" % WC_BASE, ".\\port3"],
                 define_macros=[('_BTWLIB', None)],
                 library_dirs=["%s\\Release" % WC_BASE, "%s\\Lib" % PSDK_PATH],
@@ -79,43 +82,38 @@ elif sys.platform.startswith('linux'):
                         libraries = ['bluetooth'],
                         #extra_compile_args=['-O0'],
                         sources = ['bluez/btmodule.c', 'bluez/btsdp.c'])
-    mods = [ mod1 ]
+    ext_modules.append(mod1)
 
 elif sys.platform.startswith("darwin"):
-    # On Mac, install LightAquaBlue framework
-    # if you want to install the framework somewhere other than /Library/Frameworks
-    # make sure the path is also changed in LightAquaBlue.py (in src/mac)
-    packs.append('lightblue')
-    pack_dir = { 'lightblue': 'osx' }
-    if "install" in sys.argv:
-        install_req = ['pyobjc-core>=3.1', 'pyobjc-framework-Cocoa>=3.1']
-        # Change to LightAquaBlue framework dir.
-        os.chdir("osx/LightAquaBlue")
-
-        #
-        # NOTE: our solution is based on these posts:
-        #  - http://stackoverflow.com/questions/22279913/how-to-install-either-pybluez-or-lightblue-on-osx-10-9-mavericks
-        #  - https://github.com/0-1-0/lightblue-0.4/issues/7
-        # We need to verify that the arch(itecture) param is specified accurately 
-        # otherwise the compilation will fail.
-        #
-        arch = None
-        if "NATIVE_ARCH_ACTUAL" in os.environ:
-            arch = "$(NATIVE_ARCH_ACTUAL)"
-        elif "64bit" in platform.architecture():
-            arch = "x86_64"
-        elif "32bit" in platform.architecture():
-            arch = "i386"
-        else:
-            raise Exception("Unrecognized architecture")
-
-        build_str = "xcodebuild install -arch '%s' -target LightAquaBlue -configuration Release DSTROOT=/ INSTALL_PATH=/Library/Frameworks DEPLOYMENT_LOCATION=YES" % (arch)
-        os.system(build_str)
-
-        # Change back to top-level (need this otherwise setup.py script gets confused on install?).
-        os.chdir("../..")
-
-        mods = []
+    packages.append('lightblue')
+    package_dir['lightblue'] = 'osx'
+    install_requires += ['pyobjc-core>=3.1', 'pyobjc-framework-Cocoa>=3.1']
+    zip_safe = False
+    
+    # FIXME: This is inelegant, how can we cover the cases?
+    if 'install' in sys.argv or 'bdist' in sys.argv or 'bdist_egg' in sys.argv:
+        # Build the framework into osx/
+        import subprocess
+        subprocess.check_call([
+            'xcodebuild', 'install',
+            '-project', 'osx/LightAquaBlue/LightAquaBlue.xcodeproj',
+            '-scheme', 'LightAquaBlue',
+            'DSTROOT=' + os.path.join(os.getcwd(), 'osx'),
+            'INSTALL_PATH=/',
+            'DEPLOYMENT_LOCATION=YES',
+        ])
+        
+        # We can't seem to list a directory as package_data, so we will
+        # recursively add all all files we find
+        package_data['lightblue'] = []
+        for path, _, files in os.walk('osx/LightAquaBlue.framework'):
+            for f in files:
+                include = os.path.join(path, f)[4:]  # trim off osx/
+                package_data['lightblue'].append(include)
+    
+        # This should allow us to use the framework from an egg [untested]
+        eager_resources.append('osx/LightAquaBlue.framework')
+        
 else:
     raise Exception("This platform (%s) is currently not supported by pybluez."
                     % sys.platform)
@@ -127,8 +125,8 @@ setup(name='PyBluez',
       author="Albert Huang",
       author_email="ashuang@alum.mit.edu",
       url="http://karulis.github.io/pybluez/",
-      ext_modules=mods,
-      packages=packs,
+      ext_modules=ext_modules,
+      packages=packages,
 # for the python cheese shop
       classifiers=['Development Status :: 4 - Beta',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
@@ -139,10 +137,13 @@ setup(name='PyBluez',
       download_url='https://github.com/karulis/pybluez',
       long_description='Bluetooth Python extension module to allow Python "\
                 "developers to use system Bluetooth resources. PyBluez works "\
-                "with GNU/Linux and Windows XP.',
+                "with GNU/Linux, macOS, and Windows XP.',
       maintainer='Piotr Karulis',
       license='GPL',
       extras_require={'ble': ['gattlib==0.20150805']},
-      package_dir=pack_dir,
+      package_dir=package_dir,
       use_2to3=True,
-      install_requires=install_req)
+      install_requires=install_requires,
+      package_data=package_data,
+      eager_resources=eager_resources,
+      zip_safe=zip_safe)
