f981e23e0b243023e9fd987e5b944df2d5d0d3f1
Bill Crawford <3253399+beadysea@users.noreply.github.com>
2019-07-23 21:57:47 +0100

    Fix Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ release = 'master'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
