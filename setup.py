924ea22ec527066a0cd87166863f6a5440d6c46a
hiaselhans <simon.klemenc@gmail.com>
2020-11-09 11:11:46 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ else:
 
 
 setup(name='pybluez2',
-      version='0.34g',
+      version='0.34',
       description='Bluetooth Python extension module',
       author="Albert Huang",
       author_email="ashuang@alum.mit.edu",
