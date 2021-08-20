449ec20fe42314b43c5053df241609d7bf198ec4
hiaselhans <simon.klemenc@gmail.com>
2021-08-20 14:43:42 +0200

    fix delegate sync_write

--- bluetooth/macos/btsocket/delegate.py
+++ bluetooth/macos/btsocket/delegate.py
@@ -17,10 +17,7 @@ objc.registerMetaDataForSelector(
     b"writeSync:length:",   # Selector name
     {
        "arguments": {
-         2: { "type_modifier": objc._C_ID, "c_array_length_in_arg": 4 },
-         3: { "type_modifier": b"Q"},
-
-         
+         2: { "type_modifier": objc._C_IN, "c_array_length_in_arg": 3},
        }
      }
 )
@@ -64,80 +61,9 @@ class BluezBluetoothChannelDelegate(NSObject):
     @staticmethod
     def synchronouslyWriteData_toRFCOMMChannel_(data, channel):
         #     return [channel writeSync:(void *)[data bytes] length:[data length]];
-        #data = bytearray(data)
-        print("send", data, type(data))
-        return channel.writeSync_length_(data, len(data))
+        return channel.writeSync_length_(data.bytes(), len(data))
 
     @staticmethod
     def synchronouslyWriteData_toL2CAPChannel_(data, channel):
-        #data = bytes(data)
-        return channel.writeSync_length_(data, len(data))
+        return channel.writeSync_length_(data.bytes(), len(data))
         
-
-
-# static SEL channelDataSelector;
-# static SEL channelClosedSelector;
-
-
-# @implementation BBBluetoothChannelDelegate
-
-# + (void)initialize
-# {
-#     channelDataSelector = @selector(channelData:data:);
-#     channelClosedSelector = @selector(channelClosed:);
-# }
-
-# - (id)initWithDelegate:(id)delegate
-# {
-#     self = [super init];
-#     m_delegate = delegate;
-#     return self;
-# }
-
-# - (void)rfcommChannelData:(IOBluetoothRFCOMMChannel *)rfcommChannel
-#                      data:(void *)dataPointer
-#                    length:(size_t)dataLength
-# {
-#     if (m_delegate && [m_delegate respondsToSelector:channelDataSelector]) {
-#         [m_delegate channelData:rfcommChannel
-#                            data:[NSData dataWithBytes:dataPointer length:dataLength]];
-#     }
-# }
-
-# - (void)rfcommChannelClosed:(IOBluetoothRFCOMMChannel *)rfcommChannel
-# {
-#     if (m_delegate && [m_delegate respondsToSelector:channelClosedSelector]) {
-#         [m_delegate channelClosed:rfcommChannel];
-#     }
-# }
-
-# - (void)l2capChannelData:(IOBluetoothL2CAPChannel *)l2capChannel
-#                     data:(void *)dataPointer
-#                   length:(size_t)dataLength
-# {
-#     if (m_delegate && [m_delegate respondsToSelector:channelDataSelector]) {
-#         [m_delegate channelData:l2capChannel
-#                            data:[NSData dataWithBytes:dataPointer length:dataLength]];
-#     }
-# }
-
-# - (void)l2capChannelClosed:(IOBluetoothL2CAPChannel *)l2capChannel
-# {
-#     if (m_delegate && [m_delegate respondsToSelector:channelClosedSelector]) {
-#         [m_delegate channelClosed:l2capChannel];
-#     }
-# }
-
-# + (IOReturn)synchronouslyWriteData:(NSData *)data
-#                    toRFCOMMChannel:(IOBluetoothRFCOMMChannel *)channel
-# {
-#     return [channel writeSync:(void *)[data bytes] length:[data length]];
-# }
-
-# + (IOReturn)synchronouslyWriteData:(NSData *)data
-#                     toL2CAPChannel:(IOBluetoothL2CAPChannel *)channel
-# {
-#     return [channel writeSync:(void *)[data bytes] length:[data length]];
-# }
-
-# @end
