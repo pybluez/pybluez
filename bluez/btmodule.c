8f9e31633d5798d1f2df7e0a44600aa177cb882f
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-06-13 15:12:30 +0900

    Use PyBytes_FromStringAndSize because it may not always be interpreted as Unicode

--- bluez/btmodule.c
+++ bluez/btmodule.c
@@ -2052,7 +2052,7 @@ bt_hci_send_req(PyObject *self, PyObject *args, PyObject *kwds)
 
     if( err< 0 ) return socko->errorhandler();
 
-    return PyUnicode_FromStringAndSize(rparam, req.rlen);
+    return PyBytes_FromStringAndSize(rparam, req.rlen);
 }
 PyDoc_STRVAR(bt_hci_send_req_doc,
 "hci_send_req(sock, ogf, ocf, event, rlen, params=None, timeout=0)\n\
